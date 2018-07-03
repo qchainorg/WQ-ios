8ebc82bc0caf353b6e231ce5a2abc718b433a0e6
Jelly Foo <xcode461@gmail.com>
2018-07-03 13:48:02 +0800

    for update

---
+++ Qlink/Main/NewGuide/GuideClickWalletView.h
@@ -0,0 +1,13 @@
+//
+//  GuideClickWalletView.h
+//  Qlink
+//
+//  Created by Jelly Foo on 2018/7/3.
+//  Copyright © 2018年 pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GuideClickWalletView : UIView
+
+@end
---
+++ Qlink/Main/NewGuide/GuideClickWalletView.m
@@ -0,0 +1,21 @@
+//
+//  GuideClickWalletView.m
+//  Qlink
+//
+//  Created by Jelly Foo on 2018/7/3.
+//  Copyright © 2018年 pan. All rights reserved.
+//
+
+#import "GuideClickWalletView.h"
+
+@implementation GuideClickWalletView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
